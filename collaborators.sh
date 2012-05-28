4a2541c7d983b85d1cc292de1a33dac78c239898
Joey Lawrance <lawrancej@wit.edu>
2012-05-27 23:57:18 -0400

    Minor change to collaborators.sh

--- collaborators.sh
+++ collaborators.sh
@@ -11,6 +11,7 @@ if [ $# = 0 ]; then
 	echo "setup       Do git remote add for all collaborators"
 	echo "fetch       Do git fetch for all collaborators"
 	echo "clean       Do git remote rm for all collaborators"
+	echo "list        Show a list of collaborators"
 	echo ""
 	echo "Example: $0 setup"
 elif [ $1 = "fetch" ]; then
@@ -22,6 +23,8 @@ else
 			git remote add $collaborator https://github.com/$collaborator/CompilerDesign.git
 		elif [ $1 = "clean" ]; then
 			git remote rm $collaborator
+		elif [ $1 = "list" ]; then
+			echo $collaborator
 		fi
 	done
 fi
