ecfec9cb3e3e3409fa5467b572ad88dc65f6a45b
Joey Lawrance <lawrancej@wit.edu>
2012-07-01 16:15:04 -0400

    Updated generate.sh to work in windows.

--- generate.sh
+++ generate.sh
@@ -30,7 +30,7 @@ else
     if [ $1 = "total" ]; then
         # Adapted from: http://stackoverflow.com/questions/4589731/git-blame-statistics
 
-        git ls-tree -r HEAD|sed -E -e 's/^.{53}//'|while read filename; do file "$filename"; done|grep -E ': .*text'|sed -E -e 's/: .*//'|while read filename; do git blame -w "$filename"; done|sed -E -e 's/.*\((.*)[0-9]{4}-[0-9]{2}-[0-9]{2} .*/\1/' -e 's/ +$//'|sort|uniq -c|sort -nr
+        git ls-tree -r HEAD|sed -E -e 's/^.{53}//'|while read filename; do git blame -w "$filename"; done|sed -E -e 's/.*\((.*)[0-9]{4}-[0-9]{2}-[0-9]{2} .*/\1/' -e 's/ +$//'|sort|uniq -c|sort -nr
     fi
     if [ $1 = "lastweek" ]; then
         git shortlog --no-merges -s -n --since="(7days)"
