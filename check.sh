6bab81a48fb8dfa382a147a61cf1b3a819661620
Joey Lawrance <lawrancej@wit.edu>
2012-07-07 03:23:22 -0400

    Added weasel word check

--- check.sh
+++ check.sh
@@ -69,6 +69,17 @@ grep -E -r -n -i "\\b(am|are|were|being|is|been|was|be)\
     echo "Passive voice: $line"
 done
 
+# Weasel word check, adapted from the same source
+weasels="many|various|very|fairly|several|extremely\
+|exceedingly|quite|remarkably|few|surprisingly\
+|mostly|largely|huge|tiny|((are|is) a number)\
+|excellent|interestingly|significantly\
+|substantially|clearly|vast|relatively|completely"
+  
+egrep -E -r -n -i "\\b($weasels)\\b" textbook | while read line; do
+    echo "Weasel word: $line"
+done
+
 # Warn about poor phrasing
 # "^There is|^There are"
 
