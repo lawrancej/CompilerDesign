df1aceaa680f418eb84588ca29a5ae1fa0c0f954
Joey Lawrance <lawrancej@wit.edu>
2012-07-11 07:03:14 -0400

    Untested initial work in progress on install target.

--- generate.sh
+++ generate.sh
@@ -6,6 +6,37 @@ else
     section=""
 fi
 
+install_dependencies() {
+    # If it's Windows, ...
+    if [ $OSTYPE == "msys" ] || [ $OSTYPE == "cygwin" ]; then
+        echo "Installing project dependencies for Windows..."
+        # Windows: making life difficult for programmers since 1985.
+        
+    # If it's Linux, ...
+    elif [ $OSTYPE == "linux-gnu" ]; then
+        echo "Installing project dependencies for Linux..."
+        # Debian, Ubuntu, Linux Mint
+        if [ -n `which apt-get` ]; then
+            sudo apt-get install git pandoc texlive-latex-recommended inkscape dia libreoffice-draw diction
+        # Redhat, CentOS
+        elif [ -n `which yum` ]; then
+            sudo yum install git pandoc texlive texlive-latex inkscape dia libreoffice diction
+        # Does anybody use arch or Gentoo?
+        else
+            echo "Egad! What Linux distro are you using?"
+        fi
+    # If it's Mac OS X, ...
+    elif [ $OSTYPE == "darwin" ]; then
+        echo "Installing project dependencies for Mac OS X..."
+        /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+        brew install git pandoc texlive-latex
+    # Hmm, you're on your own ...
+    else
+        echo "I don't know what OS you're using. Is it BSD?"
+        echo "Install git, LaTeX, pandoc, and a diagram editor that imports/exports SVG (e.g., Inkscape, Dia)"
+    fi
+}
+
 if [ $# = 0 ]; then
     echo "Build CompilerDesign."
     echo ""
@@ -18,10 +49,12 @@ if [ $# = 0 ]; then
     echo "guide         Builds PDF project documentation."
     echo "clean         Removes the existing generated files."
     echo "check         Generate topic coverage, hyperlink, diction, style report."
+    echo
     echo "total         Generate leader board by total line contributions."
     echo "totalcommits  Generate leader board by total commits."
     echo "lastweek      Generate leader board by commits within last week."
     echo "mergestats    Generate leader board by merges within last week."
+    echo "install       Install all dependencies."
     echo ""
     echo "Example: $0 pdf"
 else
